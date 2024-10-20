0ee942ed2320e517544b9bf6179e1877450c05da
Jenisha Shrestha <164962901+Jnishha@users.noreply.github.com>
2024-10-20 21:10:04 +0545

    -overloadingcomplex.cpp

---
+++ -overloadingcomplex.cpp
@@ -0,0 +1,39 @@
+//finds difference of two complex numbers by overloading binary operator -
+#include<iostream>
+using namespace std;
+class Binary
+{
+    private:
+        int real;
+        int img;
+    public:
+        Binary()
+        {
+            real=img=0;
+        }
+        Binary(int r,int i)
+        {
+            real=r;
+            img=i;
+        }
+        Binary operator-(Binary b)
+        {
+            Binary temp;
+            temp.real = real - b.real;
+            temp.img = img - b.img;
+            return temp;
+        }
+        void display()
+        {
+            cout<<real<<" + "<<img<<"i"<<endl;
+        }
+}; 
+int main()
+{
+    Binary b1(5,3);
+    Binary b2(2,1);
+    Binary b3;
+    b3=b1-b2;
+    b3.display();
+    return 0;
+}
